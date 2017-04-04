7e5676e97f2bb0575f9916795b478f7a5e838e9f
Indyukov Sergey <SergeyI88>
2017-04-04 23:08:30 +0300

    task 4086

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,20 @@
+/**
+* Package for calculate task.
+*
+* @author Sergey Indyukov(mailto:onl.ont@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+
+package ru.job4j;
+
+public class Calculate{
+
+/**
+* Метод main, вывод строки в консоль, входные параметры массив строк.
+* @param args - аргументы из командной строки
+*/
+public static void main(String[] args){
+System.out.println("Hello world!");
+}
+}
