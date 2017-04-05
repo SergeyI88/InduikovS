2a1277d0808df3d9754f3511f151e1a6e0f7fcf5
Indyukov Sergey <SergeyI88>
2017-04-05 13:19:36 +0300

    task 185

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,35 +1,53 @@
 package ru.job4j.calculator;
-
+/**
+ * Calculate.
+ *
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculator {
+/**
+* @param result
+*/
     private double result;
-    
+/**
+* Test add.
+*@param first первый аргумент
+*@param second второй аргумент
+*/  
     public void add(double first, double second) {
         this.result = first + second;
     }
-    
-    public double getResult() {
-        return this.result;
-    }
+/**
+* Test substruct.
+*@param first первый аргумент
+*@param second второй аргумент
+*/
 	 public void substruct(double first, double second) {
         this.result = first - second;
     }
-    
-    public double getResult() {
-        return this.result;
-    }
+/**
+* Test div.
+*@param first первый аргумент
+*@param second второй аргумент
+*/
 	 public void div(double first, double second) {
         this.result = first / second;
     }
-    
-    public double getResult() {
-        return this.result;
-    }
+/**
+* Test multiple.
+*@param first первый аргумент
+*@param second второй аргумент
+*/
 	 public void multiple(double first, double second) {
         this.result = first * second;
     }
-    
-    public double getResult() {
+/**
+* Test result.
+@return result
+*/
+ public double getResult() {
         return this.result;
     }
-
 }
