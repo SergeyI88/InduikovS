923415e09ec2248e53223f8ee4ae8237c27cfc79
Indyukov Sergey <SergeyI88>
2017-04-06 13:51:59 +0300

    task 193

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+/**
+* Factorial.
+*
+* @author Sergey Indyukov (onl.ont@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+public class Factorial {
+/** Method area.
+* @param n первый аргумент
+* @return sum
+*/
+	public int calc(int n) {
+		int mul;
+		if (n <= 0) {
+			mul = 1;
+			return mul;
+		} else {
+			for (int i = 1; i <= 5; i++) {
+				mul *= i;
+			}
+		}
+	}
+}
