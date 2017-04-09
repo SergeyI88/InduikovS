17df23f0253700acad89a5fea50b4fc579ae17f7
Indyukov Sergey <SergeyI88>
2017-04-09 18:56:20 +0300

    task 4441

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -17,19 +17,19 @@ public class Turn {
 */
 	public int[] back(int[] array) {
 		int temp = 0;
-		if (array.length % 2 == 0) {
+		//if (array.length % 2 == 0) {
 			for (int i = 0; i < array.length / 2; i++) {
 				temp = array[i];
 				array[i] = array[array.length - 1 - i];
 				array[array.length - i - 1] = temp;
 			}
-		} else {
+		/*} else {
 			for (int i = 1; i < array.length / 2 + 1; i++) {
 				temp = array[i - 1];
 				array[i - 1] = array[array.length  - i];
 				array[array.length - i] = temp;
 			}
-		}
+		}*/
 		return array;
 	}
 }
