1fea64ed930c2d5add70fa83c23d6b90054814b2
Indyukov Sergey <SergeyI88>
2017-04-10 12:20:14 +0300

    task 223

--- chapter_001/src/main/java/ru/job4j/array/RotateArray.java
+++ chapter_001/src/main/java/ru/job4j/array/RotateArray.java
@@ -14,8 +14,7 @@ public class RotateArray {
 */
 	public int[][] rotate(int[][] array) {
 		int temp = 0;
-		//if (array.length == 3) {
-			for (int i = 0; i < 1; i++) {
+			for (int i = 0; i < array.length / 2; i++) {
 				for (int j = i; j < array.length - 1 - i; j++) {
 				temp = array[i][j];
                 array[i][j] = array[array.length - j - 1][i];
@@ -24,13 +23,6 @@ public class RotateArray {
                 array[j][array.length - i - 1] = temp;
 				}
 			}
-		/* else {
-				temp = array[0][0];
-				array[0][0] = array[1][0];
-				array[1][0] = array[1][1];
-				array[1][1] = array[0][1];
-				array[0][1] = temp;
-		}*/
 			//2. При решении задачи без использования дополнительного массива необходимо выполнять на каждой итерации сразу 4 шага.
 			//К примеру для массива размером 3 на 3 первая итерация будет состоять из следующих шагов [0][0] -> [2][0] -> [2][2] -> [0][2] -> [0][0].}*/
 		return array;
