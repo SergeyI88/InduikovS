a92a4b9a7593a2e777707741e0805baf88ca5d75
Indyukov Sergey <SergeyI88>
2017-04-10 16:15:17 +0300

    task 225

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -15,17 +15,18 @@ public class ArrayDuplicate {
 */
 	public int[] remove(String[] array) {
 		int c = 0; //переменная счетчик количество повторяемых слов
-			for (int i = 0; i < array.length / 2; i++) {
-				for (int j = 1; i < array.length; j++) {
-					if (array[i] == array[j]) {
-						for (int k = j; k < array.length - 1; k++) {
-							array[k] = array[k + 1];
+		 	for (int i = 0; i < array.length - 1; i++) { //проходим все элементы кроме последнего
+				for (int j = i + 1; i < array.length; j++) { // сравнивается отдельно элемент, со всеми после него 
+					if (array[i] == array[j]) { // сравниваем значение элемента внешнего цикла и внутреннего
+						for (int k = j; k < array.length - 1; k++) { // если они равны, то запускаем цикл сдвига вправо, тем самым перезаписываем элемент 
+																	 //копию следующим значением
+							array[k] = array[k + 1]; // запись
 						}
 						c++;
-						j--;
+						j--; //снова проверяем этот же элемент массива, потому как при сдвиге, мы упустим значение, этого элемента до сдвига
 					}
 				}
 			}
-		return Arrays.copyOf(array, array.length - c);
+		return Arrays.copyOf(array, array.length - c); // возвращаем массив обрезанный копиями, которые остались справа
 	} //{"Привет", "Мир", "Привет", "Супер", "Мир"};
 }
