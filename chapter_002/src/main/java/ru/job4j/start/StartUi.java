698c3fc3d3835c81df86d9e8e6f1dabc1fb9bfe5
Indyukov Sergey <SergeyI88>
2017-04-22 14:01:41 +0300

    task 784

--- chapter_002/src/main/java/ru/job4j/start/StartUi.java
+++ chapter_002/src/main/java/ru/job4j/start/StartUi.java
@@ -11,6 +11,51 @@ import ru.job4j.models.Item;
 
 public class StartUi {
 /**
+* @param;
+*/
+	private ConsoleInput input;
+/**
+* @param;
+*/
+	private Tracker tracker;
+/**
+* @param add.
+*/
+	private static final int ADD = 0;
+/**
+* @param add.
+*/
+	private static final int SHOW = 1;
+/**
+* @param add.
+*/
+	private static final int EDIT = 2;
+/**
+* @param add.
+*/
+	private static final int DEL = 3;
+/**
+* @param add.
+*/
+	private static final int FBI = 4;
+/**
+* @param add.
+*/
+	private static final int FBN = 5;
+/**
+* @param add.
+*/
+	private static final int EXIT = 6;
+/**
+* Конструктор.
+* @param input first
+* @param tracker second
+*/
+    public StartUi(ConsoleInput input, Tracker tracker) {
+        this.input = input;
+        this.tracker = tracker;
+    }
+/**
 * method showMenu.
 */
 	public static void showMenu() {
@@ -29,7 +74,7 @@ public class StartUi {
 */
 	public static void showItems(Item[] items) {
 			for (int i = 0; i < items.length; i++) {
-				System.out.println(items[i].getId() + " " + items[i].getName() + " " + items[i].getDescription() + " " + items[i].getCreate());
+				System.out.println(String.format("%s. %s %s %s", items[i].getId(), items[i].getName(), items[i].getDescription(), items[i].getCreate()));
 			}
 
 	}
@@ -39,7 +84,52 @@ public class StartUi {
 */
 	public static void showItem(Item item) {
 		if (item != null) {
-			System.out.println(item.getId() + " " + item.getName() + " " + item.getDescription() + " " + item.getCreate());
+			System.out.println(String.format("%s. %s %s %s", item.getId(), item.getName(), item.getDescription(), item.getCreate()));
+		}
+	}
+/** method init.
+*/
+	public void init() {
+		showMenu();
+		int select = this.input.select();
+		while (select < 0 || select > 6) {
+			System.out.println("Введено некорректное значение");
+		}
+		while (select != EXIT) {
+			if (select == ADD) {
+				String name = input.askName("Please, enter the item name: ");
+				String id = input.askId("Please, enter the item id: ");
+				String description = input.askDescription("Please, enter the item description: ");
+				long create = input.askCreate("Please, enter the item Create: ");
+				Item item = new Item(id, name, description, create);
+				tracker.add(item);
+			}
+			if (select == SHOW) {
+				showItems(tracker.findAll());
+			}
+			if (select == EDIT) {
+				String name = input.askName("Please, enter the item name(the names must match the name of the editabler application): ");
+				String id = input.askId("Please, enter the item id: ");
+				String description = input.askDescription("Please, enter the item description: ");
+				long create = input.askCreate("Please, enter the item Create: ");
+				Item item = new Item(id, name, description, create);
+				tracker.update(item);
+			}
+			if (select == DEL) {
+				String id = input.askId("Please, enter the item id: ");
+				Item item = new Item(id, "off", "off", 0);
+				tracker.delete(item);
+			}
+			if (select == FBI) {
+				String id = input.askId("Please, enter the item id: ");
+				showItem(tracker.findById(id));
+			}
+			if (select == FBN) {
+				String name = input.askName("Please, enter the item name: ");
+				showItems(tracker.findByName(name));
+			}
+			showMenu();
+			select = input.select();
 		}
 	}
 /**
@@ -49,46 +139,6 @@ public class StartUi {
 	public static void main(String[] args) {
 		Tracker tracker = new Tracker();
 		ConsoleInput input = new ConsoleInput();
-		showMenu();
-		int select = input.select();
-		while (select < 0 || select > 6) {
-			System.out.println("Введено некорректное значение");
-		}
-		while (select != 6) {
-			if (select == 0) {
-				String name = input.askName("Please, enter the item name: ");
-				String id = input.askId("Please, enter the item id: ");
-				String description = input.askDescription("Please, enter the item description: ");
-				long create = input.askCreate("Please, enter the item Create: ");
-				Item item = new Item(id, name, description, create);
-				tracker.add(item);
-			}
-			if (select == 1) {
-				showItems(tracker.findAll());
-			}
-			if (select == 2) {
-				String name = input.askName("Please, enter the item name(the names must match the name of the editabler application): ");
-				String id = input.askId("Please, enter the item id: ");
-				String description = input.askDescription("Please, enter the item description: ");
-				long create = input.askCreate("Please, enter the item Create: ");
-				Item item = new Item(id, name, description, create);
-				tracker.update(item);
-			}
-			if (select == 3) {
-				String id = input.askId("Please, enter the item id: ");
-				Item item = new Item(id, "off", "off", 0);
-				tracker.delete(item);
-			}
-			if (select == 4) {
-				String id = input.askId("Please, enter the item id: ");
-				showItem(tracker.findById(id));
-			}
-			if (select == 5) {
-				String name = input.askName("Please, enter the item name: ");
-				showItems(tracker.findByName(name));
-			}
-			showMenu();
-			select = input.select();
-		}
+		new StartUi(input, tracker).init();
 	}
 }
