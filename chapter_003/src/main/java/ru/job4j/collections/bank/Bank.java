d80d05aa43f808d612016f7175ade6a73705082d
Indyukov Sergey <SergeyI88>
2017-05-18 12:34:03 +0300

    task 10038

--- chapter_003/src/main/java/ru/job4j/collections/bank/Bank.java
+++ chapter_003/src/main/java/ru/job4j/collections/bank/Bank.java
@@ -23,61 +23,42 @@ public class Bank {
 
     public void addAccountToUser(User user, Account account) {
         if (user != null && map.containsKey(user)) {
-            for (User user1 : map.keySet()) {
-                if (user.equals(user1)) {
-                    user1.list.add(account);
-
-                }
-            }
+            List accounts = map.get(user);
+            accounts.add(account);
         }
     }
 
     public void deleteAccountFromUser(User user, Account account) {
         if (user != null && map.containsKey(user)) {
-            for (User user1 : map.keySet()) {
-                if (user.equals(user1)) {
-                    user1.list.remove(account);
-                }
-            }
+            List accounts = map.get(user);
+            accounts.remove(account);
         }
     }
 
     public List<Account> getUserAccounts(User user) {
-        List<Account> accounts = new ArrayList<>();
+        List accounts = null;
         if (user != null && map.containsKey(user)) {
-            for (User user1 : map.keySet()) {
-                if (user.equals(user1)) {
-                    accounts = user1.list;
-                }
-            }
+            accounts = map.get(user);
         }
         return accounts;
     }
 
     public boolean transferMoney(User srcUser, Account srcAccount, User dstUser, Account dstAccount, double amount) {
         boolean answer = false;
-        User src = null;
-        User dst = null;
-        Set<User> set = map.keySet();
-        for (User user : set) {
-            if (user.equals(srcUser)) {
-                src = user;
-            }
-            if (user != src && user.equals(dstUser)) {
-                dst = user;
-            }
-        }
-        if (map.containsKey(src) && map.containsKey(dst)) {
-            if (map.get(srcUser).contains(srcAccount) && map.get(dst).contains(dstAccount)) {
-                int indexSrc = map.get(src).indexOf(srcAccount);
-                int indexDst = map.get(dst).indexOf(dstAccount);
-                if (src != null && dst != null && src.list.get(indexSrc).value - amount >= 0) {
-                    Account accountSrc = new Account(src.list.get(indexSrc).value - amount, src.list.get(indexSrc).requisites);
-                    src.list.set(indexSrc, accountSrc);
-                    Account accountDst = new Account(dst.list.get(indexDst).value + amount, dst.list.get(indexDst).requisites);
-                    answer = true;
-                    map.get(src).set(indexSrc, accountSrc);
-                    map.get(dst).set(indexDst, accountDst);
+        if (srcUser != null && dstUser != null && map.containsKey(srcUser) && map.containsKey(dstUser)) {
+            if (map.get(srcUser).contains(srcAccount) && map.get(dstUser).contains(dstAccount)) {
+                for (Account account : map.get(srcUser)) {
+                    if (account.equals(srcAccount)) {
+                        if (account.value - amount >= 0) {
+                            account.value -= amount;
+                            answer = true;
+                        }
+                    }
+                }
+                for (Account account : map.get(dstUser)) {
+                    if (account.equals(dstAccount) && answer) {
+                        account.value += amount;
+                    }
                 }
             }
         }
