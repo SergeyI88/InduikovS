52495be51405d17d0e6c111eeb3b8ccb69122c3b
Indyukov Sergey <SergeyI88>
2017-05-28 17:21:04 +0300

    task 152

--- chapter_004/src/main/java/Iterator/IteratorIterator.java
+++ chapter_004/src/main/java/Iterator/IteratorIterator.java
@@ -1,7 +1,71 @@
 package Iterator;
 
+import java.util.Iterator;
+
 /**
  * Created by admin on 27.05.2017.
  */
-public class IteratorIterator {
+public class IteratorIterator implements Iterator<Iterator<Integer>> {
+   /* public final class ClassConvert {
+        public Iterator<Integer> convert(Iterator<Iterator<Integer>> iterator) {
+            IteratorOneArray iteratorOneArray;
+            int index = 0;
+            int indexArrayIterators = 0;
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+            return new IteratorOneArray(new Integer[]{5});
+        }
+    }*/
+
+    public static void main(String[] args) {
+        Integer[] a1 = {1, 5, 6, 76, 4};
+        Integer[] a2 = {534, 434, 65, 343, 65};
+        Integer[] a3 = {77, 88, 99, 0, 64, 900};
+
+        IteratorOneArray[] iteratorOneArray = new IteratorOneArray[3];
+        iteratorOneArray[0] = new IteratorOneArray(a1);
+        iteratorOneArray[1] = new IteratorOneArray(a2);
+        iteratorOneArray[2] = new IteratorOneArray(a3);
+        IteratorIterator iterator = new IteratorIterator(iteratorOneArray, new int[]{iteratorOneArray[0].a.length, iteratorOneArray[1].a.length, iteratorOneArray[2].a.length});
+        int i = 0;
+        while (i++ < 16) {
+            System.out.println(iterator.next().next());
+        }
+    }
+    Iterator[] iterator;
+    public int[] index;
+    public int indexArrayIterators = 0;
+
+    public IteratorIterator(Iterator[] iterator, int[] index) {
+        this.index = index;
+        this.iterator = iterator;
+    }
+
+    @Override
+    public boolean hasNext() {
+        int quantityIts = iterator.length;
+        int i = 0;
+        for (int j = 0; j < quantityIts; j++) {
+            while (iterator[j].hasNext()) {
+                iterator[j].next();
+                i++;
+            }
+        }
+        return index < i;
+    }
+
+    @Override
+    public Iterator<Integer> next() {
+        if (iterator[indexArrayIterators].hasNext()) {
+           return  iterator[indexArrayIterators];
+        } else {
+            return  iterator[++indexArrayIterators];
+        }
+    }
+
+    @Override
+    public void remove() {
+
+    }
 }
--- chapter_004/src/main/java/Iterator/IteratorOneArray.java
+++ chapter_004/src/main/java/Iterator/IteratorOneArray.java
@@ -1,7 +1,30 @@
 package Iterator;
 
+import java.util.Iterator;
+
 /**
  * Created by admin on 27.05.2017.
  */
-public class IteratorOneArray {
+public class IteratorOneArray implements Iterator<Integer> {
+    public int index = 0;
+
+    public IteratorOneArray(Integer[] a) {
+        this.a = a;
+    }
+
+    public Integer[] a;
+    @Override
+    public boolean hasNext() {
+        return index < a.length;
+    }
+
+    @Override
+    public Integer next() {
+        return a[index++];
+    }
+
+    @Override
+    public void remove() {
+
+    }
 }
