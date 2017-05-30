11a9ecd380b9933a8201f4691bb6e75f9c20db98
Indyukov Sergey <SergeyI88>
2017-05-30 19:55:58 +0300

    task 151

---
+++ chapter_004/src/test/java/Iterator/IteratorIteratorTest.java
@@ -0,0 +1,28 @@
+package Iterator;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Created by admin on 30.05.2017.
+ */
+public class IteratorIteratorTest {
+    @Test
+    public void test() {
+        Iterator<Integer> first = Arrays.asList(1, 2, 3).iterator();
+        Iterator<Integer> second = Arrays.asList(4, 5, 6).iterator();
+        Iterator<Iterator<Integer>> it = Arrays.asList(first, second).iterator();
+        Iterator<Integer> iter = IteratorIterator.convert(it);
+        Assert.assertThat(1, Is.is(iter.next()));
+        iter.next();
+        iter.next();
+        iter.next();
+        iter.next();
+        iter.next();
+        Assert.assertThat(false, Is.is(iter.hasNext()));
+    }
+}
